6c37483924a4441f9b0d644a659b50421742105e
kduong@stuy.edu <kduong@stuy.edu>
2018-09-26 20:43:04 -0400

    completed 0a (printArray) function and tested it with the class example from yesterday

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,1 +1,23 @@
-public static void printArray(int[]ary)
+public class ArrayDemo {
+
+  public static void printArray(int[]ary){
+    System.out.print("[");
+    for (int i =0; i < ary.length; i++) {
+      if (i < ary.length - 1) {
+        System.out.print(ary[i] + ", ");
+    } else {
+      System.out.print(ary[i]);
+    }
+  }
+    System.out.print("]");
+  }
+
+  public static void main(String[] args) {
+    int[] anArray;
+    anArray = new int [11];
+    for (int i =0; i < anArray.length; i++) {
+      anArray[i] = 100 + i * 10;
+    }
+    printArray(anArray);
+  }
+}
