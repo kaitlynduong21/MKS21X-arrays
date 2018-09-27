1acbb11abbfc32ff6f1aa7cb05d17c41e25bd38f
kduong@stuy.edu <kduong@stuy.edu>
2018-09-26 21:50:27 -0400

    completed 1 (countZeros2D). The function returns how many zeroes are present in the array. Also, tested it with a test case

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -28,6 +28,19 @@ public class ArrayDemo {
     }
   }
 
+  public static int countZeros2D(int[][] nums){
+    int count = 0;
+    for(int n = 0; n < nums.length; n++) {
+      for(int i = 0; i < nums[0].length; i++) {
+        if (nums[n][i] == 0) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+
   public static void main(String[] args) {
     int[] anArray;
     anArray = new int [11];
@@ -39,9 +52,10 @@ public class ArrayDemo {
     anArray1 = new int [2][3];
     for (int n = 0; n < anArray1.length; n++) {
       for(int i = 0; i < anArray1[0].length; i++) {
-        anArray1[n][i] = (i + 1) * (n + 1);
+        anArray1[n][i] = (i) * (n + 1);
       }
     }
     printArray(anArray1);
+    System.out.println("There are " + countZeros2D(anArray1) + " zeroes.");
   }
 }
